d3233a4aa457e40fb855057561a2483a7ab96e1c
aliabbani <aliabbani08@gmail.com>
2023-11-16 18:21:57 +0200

    add models controler routes error middleware

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import authListing from './routes/listing.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import authListing from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -27,6 +27,17 @@ app.listen(9000, () => {
   console.log("Server is running on port 9000!");
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-app.use('api/listing', authListing)
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("api/listing", authListing);
+
+// err is the error object - req is the request object - res is the response we send - next is the next middleware function
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});

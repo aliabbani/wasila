57864815a2e5c0830fd6229793906c819a480eb1
aliabbani <aliabbani08@gmail.com>
2023-11-16 18:16:38 +0200

    up

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import authListing from './routes/listing.route.js';
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 mongoose
@@ -16,6 +21,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(9000, () => {
   console.log("Server is running on port 9000!");
 });
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('api/listing', authListing)
---
+++ api/routes/user.route.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { deleteUser, test, updateUser,  getUserListings, getUser, getUsers } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+const router = express.Router();
+
+router.get('/test', test);
+// Verify admin to be added to get all users
+router.get('/', getUsers)
+router.get('/:id', verifyToken, getUser)
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+
+export default router;

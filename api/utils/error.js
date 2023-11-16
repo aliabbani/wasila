c3429a9a70c643eb0b3dae6dc76be867dbf201a5
aliabbani <aliabbani08@gmail.com>
2023-11-16 19:37:46 +0200

    up

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,3 +1,4 @@
+// **if we do not have error, we can customize an error by using this function**
 export const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;

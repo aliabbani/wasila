d67ed2f540301af73262642615a876f859f9994f
aliabbani <aliabbani08@gmail.com>
2023-11-16 15:32:19 +0200

    up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Profile from "./pages/Profile";
+
 const App = () => {
   return (
-    <div>
-      <div className="text-3xl font-bold underline text-red-500">app.js</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<SignIn/>}/>
+        <Route path="/logout" element={<SignOut/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/pages/SignOut.jsx
@@ -0,0 +1,9 @@
+const SignOut = () => {
+  return (
+    <div>
+      <div>SignOut</div>
+    </div>
+  );
+};
+
+export default SignOut;

76d69b938e45edb40f583e84d366e605773a98c1
aliabbani <aliabbani08@gmail.com>
2023-11-17 14:33:43 +0200

    up

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+// We use redux presist whcihc help us add the user data in the local storage
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({ user: userReducer });
 
+// setting the name of the key on the local storage
 const persistConfig = {
   key: 'root',
   storage,

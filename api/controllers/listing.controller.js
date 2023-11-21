2ee667d783286c44678c7b5878d0556f1ad1f6f6
aliabbani <aliabbani08@gmail.com>
2023-11-21 13:30:05 +0200

    up

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -73,31 +73,28 @@ export const getListing = async (req, res, next) => {
   }
 };
 
-// get all listings
+// get all listings && for the search
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
-    let offer = req.query.offer;
 
+    let offer = req.query.offer;
     if (offer === undefined || offer === 'false') {
       offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
-
     if (furnished === undefined || furnished === 'false') {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
-
     if (parking === undefined || parking === 'false') {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
-
     if (type === undefined || type === 'all') {
       type = { $in: ['sale', 'rent'] };
     }
@@ -109,6 +106,7 @@ export const getListings = async (req, res, next) => {
     const order = req.query.order || 'desc';
 
     const listings = await Listing.find({
+      // option "i" is for case insensitive search
       name: { $regex: searchTerm, $options: 'i' },
       offer,
       furnished,

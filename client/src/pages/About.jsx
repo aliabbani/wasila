844437f83081a07174f625c568e8438b09f2b141
aliabbani <aliabbani08@gmail.com>
2023-11-21 17:25:20 +0200

    add home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,39 @@
 const About = () => {
   return (
-    <div>
-      <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About the app</h1>
+      <p className="mb-4 text-slate-700">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry&apos;s standard dummy text
+        ever since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de
+        Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their
+        exact undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+        1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of
+        Good and Evil) by Cicero, written in 45 BC. This book is a treatise on
+        the theory of ethics, very popular during the Renaissance. The first
+        line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes
+        from a line in section 1.10.32.
+      </p>
+      <p className="mb-4 text-slate-700">
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+        below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de
+        Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their
+        exact original form, accompanied by English versions from the 1914
+        translation by H. Rackham.
+      </p>
     </div>
   );
 };

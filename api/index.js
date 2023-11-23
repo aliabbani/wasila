31fdbb3eab21e97b760f01707108994099357701
aliabbani <aliabbani08@gmail.com>
2023-11-23 13:42:15 +0200

    up

--- api/index.js
+++ api/index.js
@@ -23,6 +23,12 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+// app.use(cors ({
+//   origin: ["http://localhost:3000"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
+
 app.listen(9000, () => {
   console.log("Server is running on port 9000!");
 });
